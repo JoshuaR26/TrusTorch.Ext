e5d8b5140a7fc77bcea4c34040ac1a2c94eedcf3
Amritanshu Tripathy <tripathyamritanshu7@gmail.com>
2024-01-30 12:47:16 +0530

    Final extension logic

--- extension/background.js
+++ extension/background.js
@@ -1,5 +1,36 @@
-// The array of words you want to trigger the popup
+// // The array of words you want to trigger the popup
+// const domains = ["amazon.in", "flipkart.com"];
+//
+// // The function that creates the popup window
+// function createPopup() {
+//   chrome.windows.create({
+//     url: "popup.html",
+//     width: 600,
+//     height: 400,
+//     type: "popup"
+//   });
+// }
+//
+// // The function that checks if the current tab is on Amazon or Flipkart product page
+// function isProductPage(tab) {
+//   const url = new URL(tab.url);
+//   return domains.some(domain => url.hostname.endsWith(domain));
+// }
+//
+// // The listener that handles the web navigation events
+// chrome.webNavigation.onCommitted.addListener((details) => {
+//   chrome.tabs.get(details.tabId, (tab) => {
+//     if (isProductPage(tab)) {
+//       createPopup();
+//     }
+//   });
+// });
+
+// The array of domains you want to trigger the popup
 const domains = ["amazon.in", "flipkart.com"];
+
+// A variable to track the popup status
+let popupOpen = false;
 
 // The function that creates the popup window
 function createPopup() {
@@ -9,6 +40,9 @@ function createPopup() {
     height: 400,
     type: "popup"
   });
+
+  // Set popup status to open
+  popupOpen = true;
 }
 
 // The function that checks if the current tab is on Amazon or Flipkart product page
@@ -20,8 +54,15 @@ function isProductPage(tab) {
 // The listener that handles the web navigation events
 chrome.webNavigation.onCommitted.addListener((details) => {
   chrome.tabs.get(details.tabId, (tab) => {
-    if (isProductPage(tab)) {
+    if (isProductPage(tab) && !popupOpen) {
       createPopup();
     }
   });
 });
+
+// Listener to reset popup status when the popup is closed
+chrome.windows.onRemoved.addListener((windowId) => {
+  // Set popup status to closed when the popup is closed
+  popupOpen = false;
+});
+
